e40fd464ea7406804f2837bda46b9b2480c59864
maher <maherramadan2012@hotmail.com>
2023-01-05 11:23:58 -0600

    create style.css

--- app.js
+++ app.js
@@ -4,20 +4,3 @@ console.log(pets[0]);
 console.log(pets[1]);
 console.log(pets[2]);
 console.log(pets[3]);
-
-.cat {
-  font-family: "Times New Roman", Times, serif;
-  font-size: 1rem;
-  color: #FFF;
-
-}
-.dog {
-  font-family: "Times New Roman", Times, serif;
-  font-size: 1rem;
-  color: #000;
-}
-.dragon {
-  font-family: "Times New Roman", Times, serif;
-  font-size: 1rem;
-  color: #009933;
-}
